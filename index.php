fe9818e92dd07d42432119007811316c4192820b
artur-master <61171735+artur-master@users.noreply.github.com>
2021-08-11 19:21:32 -0700

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+  $response = [];
+
+  if(isset($_SERVER['HTTP_X_HELLO_WORLD'])) {
+    $header = $_SERVER['HTTP_X_HELLO_WORLD'];
+    if($header != "test") { echo("invalid header"); exit; }
+
+    $response["ip"] = $_SERVER["HTTP_X_FORWARDED_FOR"];
+
+    if(isset($_GET['name'])) {
+      $response["greeting"] = $_GET["name"];
+    }
+
+    header('Content-Type: application/json');
+	  echo(json_encode($response));
+  }
+?>
